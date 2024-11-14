9a1d7e7aac39864229c722181f3868596a94c4dd
ali <aliabdidev@gmail.com>
2024-11-15 00:20:06 +0330

    creat new components

--- app/dashboard/profile/PermissionAccordion.tsx
+++ app/dashboard/profile/PermissionAccordion.tsx
@@ -4,37 +4,74 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion"
-
+import { Button } from "@/components/ui/button"
 import PermissionToggle from "./PermissionToggle"
+import { z } from "zod"
+
+import { Form } from "@/components/ui/form"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { toast } from "@/hooks/use-toast"
+
+const FormSchema = z.object({
+    marketing_emails: z.boolean().default(false).optional(),
+    security_emails: z.boolean(),
+})
 
 function Permission() {
-    return ( 
-        <Accordion type="single" collapsible className="w-full">
-            <AccordionItem value="item-1">
-                <AccordionTrigger>Email Notifications</AccordionTrigger>
-                <AccordionContent>
-                    <PermissionToggle /> 
-                </AccordionContent>
-            </AccordionItem>
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            security_emails: true,
+        },
+    })
 
-            <AccordionItem value="item-2">
-                <AccordionTrigger>Is it styled?</AccordionTrigger>
-                <AccordionContent>
-                    Yes. It comes with default styles that matches the other
-                    components&apos; aesthetic.
-                </AccordionContent>
-            </AccordionItem>
+    function onSubmit(data: z.infer<typeof FormSchema>) {
+        toast({
+            title: "You submitted the following values:",
+            description: (
+                <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+                    <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+                </pre>
+            ),
+        })
+    }
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
+                <Accordion type="single" collapsible className="w-full">
 
-            <AccordionItem value="item-3">
-                <AccordionTrigger>Is it animated?</AccordionTrigger>
-                <AccordionContent>
-                    Yes. It&apos;s animated by default, but you can disable it if you
-                    prefer.
-                </AccordionContent>
-            </AccordionItem>
-                
-        </Accordion>
-     );
+                    <AccordionItem value="item-1">
+                        <AccordionTrigger>Email Notifications</AccordionTrigger>
+                        <AccordionContent>
+
+                            <PermissionToggle
+                                form={form}
+                            />
+                        </AccordionContent>
+                    </AccordionItem>
+
+                    <AccordionItem value="item-2">
+                        <AccordionTrigger>Is it styled?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It comes with default styles that matches the other
+                            components&apos; aesthetic.
+                        </AccordionContent>
+                    </AccordionItem>
+
+                    <AccordionItem value="item-3">
+                        <AccordionTrigger>Is it animated?</AccordionTrigger>
+                        <AccordionContent>
+                            Yes. It&apos;s animated by default, but you can disable it if you
+                            prefer.
+                        </AccordionContent>
+                    </AccordionItem>
+
+                </Accordion>
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    );
 }
 
 export default Permission;
