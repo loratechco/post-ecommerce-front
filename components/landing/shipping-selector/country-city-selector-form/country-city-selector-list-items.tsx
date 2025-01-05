d30cc83a35b96dd3a8c3d28e1113229e2f3c016e
ali <aliabdidev@gmail.com>
2025-01-05 19:25:02 +0330

    Refactor country-city selector logic; simplify Italia check and improve state management in CounterySitySelectorListItems

--- components/landing/shipping-selector/country-city-selector-form/country-city-selector-list-items.tsx
+++ components/landing/shipping-selector/country-city-selector-form/country-city-selector-list-items.tsx
@@ -17,71 +17,74 @@ function CounterySitySelectorListItems({
   dataOriginDestinationCountries,
   form,
 }: Props) {
-    const [isItaliaOrigin, setIsItaliaOrigin] = useState(false);
-    const [isItaliaDestention, setIsItaliaDestention] = useState(false);
-    const [areBothItalia, setAreBothItalia] = useState(false); 
-    
-    const checkIsItalia = ({
-      countryName,
-      setState,
-    }: {
-      countryName: string;
-      setState: (value: boolean) => void;
-    }) => {
-      countryName?.toLowerCase() === "italia" ? setState(true) : setState(false);
-    };
-    
-    useEffect(() => {
-      if (isItaliaOrigin && isItaliaDestention) {
-        setAreBothItalia(false);
-      } else {
-        setAreBothItalia(true);
-      }
-    }, [isItaliaOrigin, isItaliaDestention]);
-    
-    return (
-      <div className="flex items-center justify-center max-lg:flex-col gap-3">
-        {/* Origin */}
-        <CountryCitySelector
-          disabled={areBothItalia}
-          selectedCountry={(countryName: string) => {
-            checkIsItalia({ countryName, setState: setIsItaliaOrigin });
-          }}
-          getSearchCityValue={(value) =>
-            dispatch({ type: "SET_DEBOUNCE_ORIGIN_CITY", payload: value })
-          }
-          cityData={citySearchData?.originCityData}
-          countries={
-            dataOriginDestinationCountries?.origin_countries as Country[]
-          }
-          hookForm={form}
-          countryName="countryOrigin"
-          cityName="cityOrigin"
-        />
-    
-        {/* destination */}
-        <CountryCitySelector
-          disabled={areBothItalia}
-          selectedCountry={(countryName: string) => {
-            checkIsItalia({ countryName, setState: setIsItaliaDestention });
-          }}
-          getSearchCityValue={(value) =>
-            dispatch({
-              type: "SET_DEBOUNCE_DESTINATION_CITY",
-              payload: value,
-            })
-          }
-          cityData={citySearchData?.destinationCityData}
-          countries={
-            dataOriginDestinationCountries?.destination_countries as Country[]
-          }
-          hookForm={form}
-          countryName="countryDestination"
-          cityName="cityDestination"
-        />
-      </div>
-    );
-    
+  const [isItaliaOrigin, setIsItaliaOrigin] = useState(false);
+  const [isItaliaDestention, setIsItaliaDestention] = useState(false);
+  const [areBothItalia, setAreBothItalia] = useState(false);
+
+  const checkIsItalia = ({
+    countryName,
+    setState,
+  }: {
+    countryName: string;
+    setState: (value: boolean) => void;
+  }) => {
+    if (countryName?.toLowerCase() === "italia") {
+      setState(true);
+    } else {
+      setState(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isItaliaOrigin && isItaliaDestention) {
+      setAreBothItalia(false);
+    } else {
+      setAreBothItalia(true);
+    }
+  }, [isItaliaOrigin, isItaliaDestention]);
+
+  return (
+    <div className="flex items-center justify-center max-lg:flex-col gap-3">
+      {/* Origin */}
+      <CountryCitySelector
+        disabled={areBothItalia}
+        selectedCountry={(countryName: string) => {
+          checkIsItalia({ countryName, setState: setIsItaliaOrigin });
+        }}
+        getSearchCityValue={(value) =>
+          dispatch({ type: "SET_DEBOUNCE_ORIGIN_CITY", payload: value })
+        }
+        cityData={citySearchData?.originCityData}
+        countries={
+          dataOriginDestinationCountries?.origin_countries as Country[]
+        }
+        hookForm={form}
+        countryName="countryOrigin"
+        cityName="cityOrigin"
+      />
+
+      {/* destination */}
+      <CountryCitySelector
+        disabled={areBothItalia}
+        selectedCountry={(countryName: string) => {
+          checkIsItalia({ countryName, setState: setIsItaliaDestention });
+        }}
+        getSearchCityValue={(value) =>
+          dispatch({
+            type: "SET_DEBOUNCE_DESTINATION_CITY",
+            payload: value,
+          })
+        }
+        cityData={citySearchData?.destinationCityData}
+        countries={
+          dataOriginDestinationCountries?.destination_countries as Country[]
+        }
+        hookForm={form}
+        countryName="countryDestination"
+        cityName="cityDestination"
+      />
+    </div>
+  );
 }
 
 export default CounterySitySelectorListItems;
